9aa87e4aa91342c6bd8e3583f3fa5e2a3dcfb313
Alain Perkaz <alainperkaz@gmail.com>
2017-09-05 22:20:53 +0200

    Refactor ChessBoard

--- src/1-ProgramStructure/3-ChessBoard.js
+++ src/1-ProgramStructure/3-ChessBoard.js
@@ -10,30 +10,18 @@ const generateLineBreak = () => '\n';
 
 const isEven = number => !!((number % 2));
 
-// 1st, 3rd... line is Odd
-const generateOddLine = (size) => {
+const generateLine = (size, isLineEven) => {
   let line = '';
   let characterCount;
   for (characterCount = 1; characterCount <= size; characterCount += 1) {
     if (isEven(characterCount)) {
-      line += generateSpace();
+      line += (isLineEven)
+              ? generateHash()
+              : generateSpace();
     } else {
-      line += generateHash();
-    }
-  }
-  line += generateLineBreak();
-  return line;
-};
-
-// 2nd, 4th... line is Even
-const generateEvenLine = (size) => {
-  let line = '';
-  let characterCount;
-  for (characterCount = 1; characterCount <= size; characterCount += 1) {
-    if (isEven(characterCount)) {
-      line += generateHash();
-    } else {
-      line += generateSpace();
+      line += (isLineEven)
+              ? generateSpace()
+              : generateHash();
     }
   }
   line += generateLineBreak();
@@ -44,11 +32,7 @@ const printChessBoard = (size) => {
   let board = '';
   let lineCount;
   for (lineCount = 1; lineCount <= size; lineCount += 1) {
-    if (isEven(lineCount)) {
-      board += generateEvenLine(size);
-    } else {
-      board += generateOddLine(size);
-    }
+    board += generateLine(size, isEven(lineCount));
   }
   console.log(board);
   return true;
