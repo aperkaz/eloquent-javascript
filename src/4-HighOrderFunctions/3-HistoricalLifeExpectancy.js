169643243b84b2d52e82caa029034e63bcc7378d
aperkaz <alainperkaz@gmail.com>
2017-12-28 23:36:03 +0100

    Update HOF 3-HistoricalLifeExpectancy

--- src/4-HighOrderFunctions/3-HistoricalLifeExpectancy.js
+++ src/4-HighOrderFunctions/3-HistoricalLifeExpectancy.js
@@ -3,13 +3,44 @@ Average of people in the ancestry file. Group the results
 by century.
  */
 
-export const fromCentury = date => (
-  Math.ceil(date / 100)
+
+import ancestors from './ancestors';
+
+export const average = (array) => {
+  const plus = (a, b) => (a + b);
+  return array.reduce(plus) / array.length;
+};
+
+export const personsCentury = person => (
+  Math.ceil(person.died / 100)
 );
 
-const historicalLifeExpectancy = () => (
-  true
-);
+export const groupBy = (array, catFunction) => {
+  const groups = {};
+  array.forEach((element) => {
+    const catergory = catFunction(element);
+    if (catergory in groups) {
+      groups[catergory].push(element);
+    } else {
+      groups[catergory] = [element];
+    }
+  });
+  return groups;
+};
 
-export default historicalLifeExpectancy;
+const groupedByCentury = groupBy(ancestors, personsCentury);
+
+export const agesByCentury = (array) => {
+  const groupsOfAges = {};
+  // eslint-disable-next-line
+  for(const century in array){
+    groupsOfAges[century] = groupedByCentury[century].map(person => (
+      person.died - person.born
+    ));
+  }
+
+  return groupsOfAges;
+};
+
+export const ages = agesByCentury(groupedByCentury);
 
--- test/4-HighOrderFunctions/3-HistoricalLifeExpectancy.js
+++ test/4-HighOrderFunctions/3-HistoricalLifeExpectancy.js
@@ -1,17 +1,10 @@
 // -- dependencies --
 import { assert } from 'chai';
 // -- app --
-import historicalLifeExpectancy from '../../src/4-HighOrderFunctions/3-HistoricalLifeExpectancy';
-import {fromCentury} from '../../src/4-HighOrderFunctions/3-HistoricalLifeExpectancy';
+import { ages } from '../../src/4-HighOrderFunctions/3-HistoricalLifeExpectancy';
 
 describe(('4-HighOrderFunctions/3-HistoricalLifeExpectancy'), () => {
-  it('fromCentury, 10 century', () => {
-    assert(fromCentury(999) === 10);
-  });
-  it('fromCentury, 5 century', () => {
-    assert(fromCentury(401) === 5);
-  });
-  it('TODO', () => {
-    assert(true);
+  it('working', () => {
+    assert.deepEqual(Object.keys(ages).length, 6);
   });
 });
